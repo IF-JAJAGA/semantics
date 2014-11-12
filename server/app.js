b451233ce8f26dcc5873a5e69a94e0add5387e35
GusMonod <gustave.monod@insa-lyon.fr>
2014-11-12 17:23:34 +0100

    Enabling compression
    New README

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
+
 var favicon = require('serve-favicon');
+var compression = require('compression');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -13,9 +15,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
