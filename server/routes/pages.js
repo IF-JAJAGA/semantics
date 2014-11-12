fa466f3532e1b459abf9de9af122695d4e6c7c6e
GusMonod <gustave.monod@insa-lyon.fr>
2014-11-12 17:22:44 +0100

    Detailed view for a specific page

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('pages');
 
 var pages = [
   {uri: 'http://example.com/page1'},
@@ -9,7 +10,23 @@ var pages = [
 
 /* GET pages listing. */
 router.get('/', function(req, res) {
-  res.render('pages', {pages: pages});
+  res.render('pages', {title: 'Liste des pages', pages: pages});
+});
+
+router.get('/:id', function(req, res, next) {
+  var id = parseInt(req.params.id, 10);
+  debug('id: ' + id);
+  if (id - 1 < pages.length) {
+    res.render('page', {
+      title: 'Détail de la page ' + id,
+      page: pages[id - 1]
+    });
+  }
+  else {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
 });
 
 module.exports = router;
