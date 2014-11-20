13b665c5247c2ba3eb67d527d2da269b29b553b1
Nautigsam <aurelien.bertron@insa-lyon.fr>
2014-11-20 11:56:02 +0100

    ajoute le template de la page de recherche

--- routes/search.js
+++ routes/search.js
@@ -21,9 +21,16 @@ var
   crypto = require('crypto');
 
 router.get('/', function(req,res,next) {
-  debug('test');
-  res.write('test');
-  res.end();
+  var params = req.query;
+  debug('requête : ' + params.q);
+  if (req.accepts('text/html')) {
+    res.render('search', {title: 'Résultats de la requête', inputValue: params.q});
+  } else if (req.accepts('json')) {
+    res.set('Content-Type', 'application/json');
+    res.status('200').send(JSON.stringify({q: params.q}));
+  } else {
+    res.status('406').send('Not Acceptable');
+  }
 });
 
 module.exports = router;
