2bfa7b9d147a34ba46d6eb85ef3eff337d1bea5e
jeje <jerome.guidon@insa-lyon.fr>
2014-11-22 01:55:27 +0000

    modified:   ../testToSpotlight.json 	modified:   exampleJSON.js Avancement pour classer les sujets par apparition

--- testsJerome/exampleJSON.js
+++ testsJerome/exampleJSON.js
@@ -22,16 +22,58 @@ console.log(resultats.length);
 /*
  * Functions
  */
-function find(sujet, list){
-	for (var i = 0, len = list.length; i < len; i++) {
-		if(list[i].subject == sujet){
-			console.log(list[i].nbApparitions);
+function find(sujet, subjectsList){
+	for (var i = 0, len = subjectsList.length; i < len; i++) {
+		if(subjectsList[i].subject == sujet){
 			return i;
 		}
 	}
-	console.log("Pas trouvé");
 	return undefined;
+}
 
+function presentInBest(list, i){
+	if(list.length == 0)	return 0;
+	for(var j=0, lenj=list.length; j<lenj; j++){
+		if(list[j] == i){
+			return 1;
+			j = lenj;
+	}
+	return 0;
+	}
+}
+
+/*
+ * La fonction bestSubjects génère une liste de @param size éléments
+ * voulus classée par ordre décroissant de nombres d'apparitions dans la 
+ * liste @param subjectsList.
+ */
+function bestSubjects(subjectsList, size){
+	var toReturn=[];
+
+	var max = 0;
+	while(toReturn.length < size){
+		var maxBoucle = 0;
+		var addIndice = undefined;
+		for(var i=0, len=subjectsList.length; i<len; i++){
+			var freqTest = subjectsList[i].frequency;
+			console.log("freqTest : " + freqTest);
+			if(freqTest > maxBoucle && (max == 0 || freqTest <= max)){
+				var dejaPresent = presentInBest(toReturn, i);
+				console.log("avant if avec dejaPresent : " + dejaPresent);
+				if(dejaPresent == 0) {
+					console.log("Après if");
+					addIndice = i;
+					maxBoucle = freqTest;
+					console.log("maxBoucle : " + maxBoucle + " ; freqtest : " + freqTest);
+				}
+			}
+		}
+		console.log("addIndice : " + addIndice);
+		toReturn.push(addIndice);
+		console.log("toReturn : " + toReturn.toString());
+		if(max==0)	max = maxBoucle;
+	}
+	return toReturn;
 }
 
 /*
@@ -74,6 +116,10 @@ for(root in resultats){
 	}
 }
 
+var bestsList = bestSubjects(subjectsList, 4);
+console.log("bestsList : " + bestsList.toString());
+
 console.log("Longueur de la liste : " + subjectsList.length);
-console.log("Le troisième element " + subjectsList[2].subject + " apparait : " + subjectsList[2].frequency + " fois.")
-console.log("Le troisième element " + subjectsList[3].subject + " apparait : " + subjectsList[3].frequency + " fois.")
+for(var i=0, len=subjectsList.length ; i<len ; i++){
+	console.log("element " + i + " apparait " + subjectsList[i].frequency + " fois.");
+}
